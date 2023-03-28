79f0ef8d181e87b0d13ff9bdc6bf96dc57ceca65
kaique <kaique.mendonca@cenpros.com.br>
2023-03-28 08:31:16 -0300

    terminando envio de email

--- src/main/java/com/ms/email/controllers/EmailController.java
+++ src/main/java/com/ms/email/controllers/EmailController.java
@@ -1,7 +1,15 @@
 package com.ms.email.controllers;
 
+import com.ms.email.dtos.EmailDto;
+import com.ms.email.models.Email;
 import com.ms.email.services.EmailService;
+import jakarta.validation.Valid;
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -9,4 +17,12 @@ public class EmailController {
 
     @Autowired
     private EmailService emailService;
+
+    @PostMapping("/send-email")
+    public ResponseEntity<Email> sendingEmail(@RequestBody @Valid EmailDto emailDto) {
+        Email emailModel = new Email();
+        BeanUtils.copyProperties(emailDto, emailModel);
+        emailService.sendEmail(emailModel);
+        return new ResponseEntity<>(emailModel, HttpStatus.CREATED);
+    }
 }
